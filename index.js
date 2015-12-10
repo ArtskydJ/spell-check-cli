c01d986bfbd44c2cf9b9b517936e6ec212e94323
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-10 16:48:58 -0600

    Add usage and status codes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var enUS = require('dictionary-en-us')
 var Spellchecker = require('hunspell-spellchecker')
 
 var word = process.argv[2]
+if (!word || word[0] === '-') {
+	console.log('Usage: spell <word>')
+	process.exit(1)
+}
 
 enUS(function (err, dictionaries) {
 	if (err) throw err
@@ -11,4 +15,5 @@ enUS(function (err, dictionaries) {
 
 	var ok = spellchecker.check(word)
 	console.log(ok ? 'ok' : 'not ok')
+	process.exit(ok ? 0 : 1)
 })
