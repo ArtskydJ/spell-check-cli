5e2292ea3411b601a268b12fb39d01e43443ae66
Joseph Dykstra <josephdykstra@gmail.com>
2016-04-12 15:08:04 -0500

    Simplify test
    You can check for non-zero exit codes with `err.code`

--- test.js
+++ test.js
@@ -1,47 +1,30 @@
 var assert = require('assert')
-var execFile = require('child_process').execFile
+var cp = require('child_process')
 
-function goodSpelling(cb) {
-	execFile('node', ['index.js', 'hello'], { cwd: __dirname }, function (err, stdout, stderr) {
-		assert.ifError(err)
-		assert.equal(stderr.toString(), '')
-		assert.equal(stdout.toString(), 'ok\n')
-
-		cb()
-	}).on('close', function (statusCode) {
-		assert.equal(statusCode, 0)
-	})
-}
-
-function badSpelling(cb) {
-	execFile('node', ['index.js', 'helol'], { cwd: __dirname }, function (err, stdout, stderr) {
-		assert.ok(err)
-		assert.equal(stderr.toString(), '')
-		assert.equal(stdout.toString(), 'not ok\n')
-
-		cb()
-	}).on('close', function (statusCode) {
-		assert.equal(statusCode, 1)
-	})
-}
-
-function noArgumentsPassed(cb) {
-	execFile('node', ['index.js'], { cwd: __dirname }, function (err, stdout, stderr) {
-		assert.ok(err)
-		assert.equal(stderr.toString(), '')
-		assert.equal(stdout.toString(), 'Usage: spell <word>\n')
-
-		cb()
-	}).on('close', function (statusCode) {
-		assert.equal(statusCode, 1)
-	})
-}
-
-
-goodSpelling(function () {
-	badSpelling(function () {
-		noArgumentsPassed(function () {
-			console.log('ok')
-		})
-	})
+cp.execFile('node', ['index.js', 'hello'], { cwd: __dirname }, function (err, stdout, stderr) {
+	assert.ifError(err)
+	assert.equal(stderr.toString(), '')
+	assert.equal(stdout.toString(), 'ok\n')
+	finish()
 })
+
+cp.execFile('node', ['index.js', 'helol'], { cwd: __dirname }, function (err, stdout, stderr) {
+	assert.ok(err)
+	assert.equal(err.code, 1)
+	assert.equal(stderr.toString(), '')
+	assert.equal(stdout.toString(), 'not ok\n')
+	finish()
+})
+
+cp.execFile('node', ['index.js'], { cwd: __dirname }, function (err, stdout, stderr) {
+	assert.ok(err)
+	assert.equal(err.code, 1)
+	assert.equal(stderr.toString(), '')
+	assert.equal(stdout.toString(), 'Usage: spell <word>\n')
+	finish()
+})
+
+var finishedCount = 0
+function finish() {
+	if (++finishedCount >= 3) console.log('ok')
+}
